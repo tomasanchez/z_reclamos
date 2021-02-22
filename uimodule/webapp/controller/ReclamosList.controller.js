80d6c68e8473d30c9d4fc255a5f4cadedafd8a4c
Matias Coronel <coronelmatiasn@gmail.com>
2021-02-22 19:37:27 +0000

    Initialize repository with easy-ui5

---
+++ uimodule/webapp/controller/ReclamosList.controller.js
@@ -0,0 +1,7 @@
+sap.ui.define([
+  "profertil/listadoReclamos/controller/BaseController"
+], function(Controller) {
+  "use strict";
+
+  return Controller.extend("profertil.listadoReclamos.controller.ReclamosList", {});
+});
